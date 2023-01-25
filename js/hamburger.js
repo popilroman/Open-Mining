8730e3e3a3abe7cde69eec3ea4ca72ed954f6312
Popil Roman <popil.roman23@mail.ru>
2023-01-25 09:42:48 +0500

    hamburger.js Изменил объявление переменных через const

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Функция, которая проверяет загрузку всего контента страницы
     window.addEventListener("load", function() {
         
-        var menuHamburger = document.querySelector(".menu__hamburger");
-        var menu = this.document.querySelector(".menu");
-        var menuSpan = this.document.querySelector(".menu__span");
+        const menuHamburger = document.querySelector(".menu__hamburger");
+        const menu = this.document.querySelector(".menu");
+        const menuSpan = this.document.querySelector(".menu__span");
 
         menuHamburger.addEventListener("click", function() {
             menu.classList.toggle("menu_visible");   
